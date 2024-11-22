4b1c209f329715936ba21a3598b35954c47cc5f2
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2024-11-22 00:37:43 -0500

    bg min-height

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -17,24 +17,6 @@ export default function Root(props) {
       <div id="bg">
         <NavBar />
         <Outlet className="fg" id="fg" />
-        {/* <div className="gradient-bg">
-          <svg xmlns='https://www.w3.org/2000/svg'>
-            <defs>
-              <filter id="goo">
-                <feGaussianBlur in="SourceGraphic" stdDeviation='10' result="blur" />
-                <feColorMatrix in="blur" mode='matrix' values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 18 -8" result="goo" />
-                <feBlend in="SourceGraphic" in2='goo' />
-              </filter>
-            </defs>
-          </svg>
-          <div className="gradients-container">
-            <div className="g1"></div>
-            <div className="g2"></div>
-            <div className="g3"></div>
-            <div className="g4"></div>
-            <div className="g5"></div>
-          </div>
-        </div> */}
         <div className="bg floating" id="bg-elipse-1"></div>
         <div className="bg floating" id="bg-elipse-2"></div>
         <div className="bg bg-square-accent floating" id="bg-square-accent-1"></div>
